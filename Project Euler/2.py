4c7976f4710745b1021f15b9c345204d9cff3ab1
Taylor Edmiston <tedmiston@gmail.com>
2015-11-22 16:24:12 -0500

    Add Project Euler #2 - Fibonnaci sum
    Note: Solved on 11/17/15.

---
+++ Project Euler/2.py
@@ -0,0 +1,51 @@
+"""
+Sum the even-valued terms in the Fibnonnaci sequence over (1, 4000000).
+
+The trick here is definitely memoization to avoid blowing up the stack with recursive calls.
+"""
+
+memo = {}
+
+
+def memoize(func):
+    """Store previously computed numbers for performance."""
+    def inner(n):
+        return memo[n] if n in memo else func(n)
+    return inner
+
+
+@memoize
+def fib(n):
+    """Return the nth number in the Fibonacci sequence."""
+    if n == 0:
+        v = 0
+    elif n == 1:
+        v = 1
+    else:
+        v = fib(n-1) + fib(n-2)
+    memo[n] = v
+    return v
+
+
+def even(n):
+    """Return True if n is even."""
+    return n % 2 == 0
+
+
+def main():
+    STARTING_TERM = 1
+    MAX_VALUE = 4000000
+
+    result = []
+    i = STARTING_TERM
+    while fib(i) <= MAX_VALUE:
+        result.append(fib(i))
+        i += 1
+
+    evens = [i for i in result if even(i)]
+    print sum(evens)
+    assert sum(evens) == 4613732
+
+
+if __name__ == '__main__':
+    main()
