6dd777dfad30096ff31b4ca91448a932c1604d90
Taylor Edmiston <tedmiston@gmail.com>
2015-11-29 01:44:20 -0500

    Properly escape %s in PE #10

--- Project Euler/10.py
+++ Project Euler/10.py
@@ -8,12 +8,12 @@ of primes so far. This worked well up to a max of 250k (~30 seconds), but at
 ~1800s (= 30 min).
 
 I refactored the prime checking out of main() into is_prime(...) which somehow
-reduced tottime by ~50% in each case, despite having many more function calls
+reduced tottime by ~50%% in each case, despite having many more function calls
 now. My guess is some kind of caching of redundant calls by the interpreter,
 which hints to a possible optimization.
 
 Now I can do a max of 500k in 60s, and 1M in 212s, but extrapolating that 2M
-would take ~740s, far longer than my preferred 60s max. At 1M, 99.9% of
+would take ~740s, far longer than my preferred 60s max. At 1M, 99.9%% of
 tottime is spent (unsuprisingly) in is_prime(...), so I wonder which checks I
 can avoid to speed it up.
 
@@ -21,7 +21,7 @@ The Wikipedia article for "Prime number" [1] points out that 'trial division',
 the technique I've applied without knowing its name, must only loop over n
 where 2 <= n <= sqrt(n). Though this method is "slow", it's fast enough, and
 simple. It drops my upper wall clock time significantly: 500k in 1s; 1M in 3s
-(a ~99% reduction); and 2M in 6s. I like it.
+(a ~99%% reduction); and 2M in 6s. I like it.
 
 For MAX=2M, the sum is 142913828922.
 
