61d6ceffa37cba63032c926debd7eb5ed3a2e312
Taylor Edmiston <tedmiston@gmail.com>
2016-10-10 17:50:46 -0400

    Simplify Codewars - Sum of the first nth term of Series

--- Codewars/Sum of the first nth term of Series/solution.py
+++ Codewars/Sum of the first nth term of Series/solution.py
@@ -2,17 +2,6 @@
 https://www.codewars.com/kata/sum-of-the-first-nth-term-of-series
 """
 
-from decimal import Decimal
-
-
 def series_sum(n):
-    if n == 0:
-        sum_ = 0
-    else:
-        sum_ = 1
-        for i in range(n-1):
-            nth_value = 1 / (4+3*i)
-            sum_ += nth_value
-
-    sum_str = str(round(Decimal(sum_), 2))
-    return sum_str
+    sum_ = sum([1 / (i*3-2) for i in range(1, n+1)])
+    return '%.2f' % sum_
