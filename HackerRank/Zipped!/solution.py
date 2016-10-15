4c65d96a94943d3eda217ba5151794ac0370445c
Taylor Edmiston <tedmiston@gmail.com>
2016-10-15 16:18:14 -0400

    Move Zipped! code into a main function
    Required for unit testing

--- HackerRank/Zipped!/solution.py
+++ HackerRank/Zipped!/solution.py
@@ -2,14 +2,19 @@
 Zipped! - https://www.hackerrank.com/challenges/zipped
 """
 
-n, x = input().split()
-n = int(n)  # student count
-x = int(x)  # subject count
-grades = [[float(i) for i in input().split()] for i in range(x)]
 
-grades_by_student = zip(*grades)
-average_by_student = [sum(i) / len(i) for i in list(grades_by_student)]
-average_by_student = [round(i, 1) for i in average_by_student]
+def main():
+    n, x = input().split()
+    n = int(n)  # student count
+    x = int(x)  # subject count
+    grades = [[float(i) for i in input().split()] for i in range(x)]
 
-for i in average_by_student:
-    print(i)
+    grades_by_student = zip(*grades)
+    average_by_student = [sum(i) / len(i) for i in list(grades_by_student)]
+    average_by_student = [round(i, 1) for i in average_by_student]
+
+    for i in average_by_student:
+        print(i)
+
+if __name__ == '__main__':
+    main()
