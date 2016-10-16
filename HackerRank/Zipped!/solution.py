3c2e02437c11507c65f5697fe709aa67527f2760
Taylor Edmiston <tedmiston@gmail.com>
2016-10-15 20:16:06 -0400

    Use map over list comprehension

--- HackerRank/Zipped!/solution.py
+++ HackerRank/Zipped!/solution.py
@@ -7,8 +7,7 @@ def main():
     n, x = input().split()
     n = int(n)  # student count
     x = int(x)  # subject count
-    grades = [[float(i) for i in input().split()] for i in range(x)]
-
+    grades = [list(map(float, input().split())) for i in range(x)]
     grades_by_student = zip(*grades)
     average_by_student = [sum(i) / len(i) for i in list(grades_by_student)]
     average_by_student = [round(i, 1) for i in average_by_student]
